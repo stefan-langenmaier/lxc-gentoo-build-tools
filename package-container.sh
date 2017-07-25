97930369dcb6b9d5da41c720ff0980a903aea68b
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2017-07-25 11:25:33 -0400

    accelerate the packaging with two threads

--- package-container.sh
+++ package-container.sh
@@ -8,5 +8,5 @@ mountpoint -q /mnt/full-root/ || mount /mnt/full-root
 btrfs sub snap /mnt/full-root/vols/${CONTAINER_NAME}/rootfs /mnt/full-root/vols/${CONTAINER_NAME}/rootfs-temp
 btrfs property set -ts /mnt/full-root/vols/${CONTAINER_NAME}/rootfs-temp ro true
 mkdir -p .packaged-subvolumes
-btrfs send -vvv /mnt/full-root/vols/${CONTAINER_NAME}/rootfs-temp | xz > .packaged-subvolumes/${CONTAINER_NAME}.xz
+btrfs send -vvv /mnt/full-root/vols/${CONTAINER_NAME}/rootfs-temp | xz --threads=2 > .packaged-subvolumes/${CONTAINER_NAME}.xz
 btrfs sub delete /mnt/full-root/vols/${CONTAINER_NAME}/rootfs-temp
