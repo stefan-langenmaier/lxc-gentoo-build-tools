21164cd2b6748a70ebddd88c2b3c8a5a134657e0
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2018-06-20 16:27:57 -0400

    updated the run script for nextcloud

--- docker/nextcloud/run/run.sh
+++ docker/nextcloud/run/run.sh
@@ -4,16 +4,18 @@ docker run \
 	-v /usr/portage:/usr/portage:ro \
         -v /usr/portage/distfiles:/usr/portage/distfiles:rw \
         -v /data/container-packages:/usr/portage/packages:rw \
-        -v /data/nextcloud-config/letsencrypt:/etc/letsencrypt:rw \
-        -v /mnt/full-data/vols/nextcloud-data-migration:/data/nextcloud:rw \
-        -v /data/nextcloud-config/nextcloud/config.php:/var/www/localhost/htdocs/nextcloud/config/config.php:rw \
-        -v /data/nextcloud-config/nginx/nginx.conf:/etc/nginx/nginx.conf:rw \
-        -v /data/nextcloud-config/php/fpm-php7.1/fpm.d/www.conf:/etc/php/fpm-php7.1/fpm.d/www.conf:rw \
+        -v /mnt/full-data/vols/nextcloud-config/letsencrypt:/etc/letsencrypt:rw \
+        -v /mnt/full-data/vols/nextcloud-data:/data/nextcloud:rw \
+        -v /mnt/full-data/vols/nextcloud-config/nextcloud/config.php:/var/www/localhost/htdocs/nextcloud/config/config.php:rw \
+        -v /mnt/full-data/vols/nextcloud-config/nginx/nginx.conf:/etc/nginx/nginx.conf:rw \
+        -v /mnt/full-data/vols/nextcloud-config/php/fpm-php7.1/fpm.d/www.conf:/etc/php/fpm-php7.1/fpm.d/www.conf:rw \
+        -v /mnt/full-data/vols/nextcloud-config/mysql/my.cnf:/etc/mysql/my.cnf:rw \
+        -v /mnt/full-data/vols/nextcloud-db:/var/lib/mysql:rw \
 	-p 443:443 \
 	-p 80:80 \
 	--rm \
 	-d \
-        --name "nextcloud-emilie" \
-        "stefan-langenmaier/nextcloud-emilie:2018-05-13" \
+        --name "nextcloud" \
+        "slangenmaier/nextcloud:2018-05-15" \
         /sbin/init
 
