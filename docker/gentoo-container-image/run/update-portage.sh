c845248a70d2ae15eccc9b45916d5a715eb47e8f
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2018-10-08 18:55:21 +0000

    updated container name

--- docker/gentoo-container-image/run/update-portage.sh
+++ docker/gentoo-container-image/run/update-portage.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 set -e
 set -x
-#	--privileged \
-#	-v /sys/fs/cgroup:/sys/fs/cgroup:rw \
 
 docker run  \
 	--rm \
 	--tmpfs /run \
 	-v /usr/portage:/usr/portage:rw \
-	--entrypoint="/sbin/init" \
-	--name "gentoo-container" \
+	--name "update-portage" \
 	"slangenmaier/gentoo-container-image:latest" \
 		emerge --sync
-
-#docker commit \
-#	"nextcloud-builder" \
-#	"stefan-langenmaier/nextcloud:${DATE}"
-
-#docker rm "nextcloud-builder"
