4b17c351678d02bab64a187a3fd8562c4c150217
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2018-10-08 23:04:52 +0000

    removed interactive part

--- docker/gentoo-base-image/build/build.sh
+++ docker/gentoo-base-image/build/build.sh
@@ -4,20 +4,18 @@ set -x
 
 cd "$(dirname "$0")"
 
-DATE=`date +%Y-%m-%d`
+DATE=`date +%Y-%m`
 
-docker run -it \
+docker run \
 	--cap-add SYS_PTRACE \
 	-v /usr/portage:/usr/portage:ro \
 	-v /usr/portage/distfiles:/usr/portage/distfiles:rw \
 	-v /mnt/full-data/vols/cuboxi-packages:/usr/portage/packages:rw \
 	--entrypoint="/bin/bash" \
 	--name "gentoo-base-container" \
-	"slangenmaier/gentoo-base-image:latest"
-#	"slangenmaier/stage3-arm-armv7a_hardfp:latest"
+	"slangenmaier/gentoo-base-image:latest" \
+		emerge -uDN world
 
-#docker commit \
-#	"nextcloud-builder" \
-#	"slangenmaier/nextcloud:${DATE}"
-#
-#docker rm "nextcloud-builder"
+docker export gentoo-base-container | docker import - "slangenmaier/gentoo-base-image:latest"
+
+docker tag "slangenmaier/gentoo-base-image:latest" "slangenmaier/gentoo-base-image:${DATE}"
