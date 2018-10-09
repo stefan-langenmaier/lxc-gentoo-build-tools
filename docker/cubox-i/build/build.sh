b9977e07c30e7160edec7f918707203fba8a86a8
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2018-10-09 21:26:11 +0000

    update cubox-i build

--- docker/cubox-i/build/build.sh
+++ docker/cubox-i/build/build.sh
@@ -10,18 +10,17 @@ docker build . -t "internal/cubox-i-builder"
 
 docker rm "cubox-i-builder" || true
 
-docker run -it \
+docker run \
 	--cap-add SYS_PTRACE \
 	-v /usr/portage:/usr/portage:ro \
 	-v /usr/portage/distfiles:/usr/portage/distfiles:rw \
-	-v /data/cuboxi-packages:/usr/portage/packages:rw \
-	-entrypoint="/bin/bash" \
+	-v /mnt/full-data/vols/cuboxi-packages:/usr/portage/packages:rw \
 	--name "cubox-i-builder" \
 	"internal/cubox-i-builder" \
 		bash /container-specific-setup.sh
 
 docker commit \
 	"cubox-i-builder" \
-	"slangenmaier/cubox-i:${DATE}"
+	"slangenmaier/cubox-i:latest"
 
-#docker rm "nextcloud-builder"
+docker tag "slangenmaier/cubox-i:latest" "slangenmaier/cubox-i:${DATE}"
