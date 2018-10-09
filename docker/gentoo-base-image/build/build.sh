a41ec6d1daa5b6ab6b017cb88721b607dca36f4b
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2018-10-09 05:28:40 +0000

    add /run folder folder base image build

--- docker/gentoo-base-image/build/build.sh
+++ docker/gentoo-base-image/build/build.sh
@@ -10,6 +10,7 @@ docker rm gentoo-base-container || true
 
 docker run \
 	--cap-add SYS_PTRACE \
+	--tmpfs /run \
 	-v /usr/portage:/usr/portage:ro \
 	-v /usr/portage/distfiles:/usr/portage/distfiles:rw \
 	-v /mnt/full-data/vols/cuboxi-packages:/usr/portage/packages:rw \
