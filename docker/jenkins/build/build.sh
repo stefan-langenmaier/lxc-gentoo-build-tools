148f97f441b83b30369ba2b566e6bd40ebdf839d
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2018-10-09 15:51:15 +0000

    update jenkins

--- docker/jenkins/build/build.sh
+++ docker/jenkins/build/build.sh
@@ -10,8 +10,9 @@ docker build . -t "internal/jenkins-builder:${DATE}"
 
 docker rm "jenkins-builder" || true
 
-docker run -it \
+docker run  \
 	--cap-add SYS_PTRACE \
+	--tmpfs /run \
 	-v /usr/portage:/usr/portage:ro \
 	-v /usr/portage/distfiles:/usr/portage/distfiles:rw \
 	-v /mnt/full-data/vols/cuboxi-packages:/usr/portage/packages:rw \
@@ -25,5 +26,3 @@ docker commit \
 	"slangenmaier/jenkins:latest"
 
 docker tag "slangenmaier/jenkins:latest" "slangenmaier/jenkins:${DATE}"
-
-#docker rm "jenkins-builder"
