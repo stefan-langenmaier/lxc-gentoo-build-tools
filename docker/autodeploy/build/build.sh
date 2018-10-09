413aec5317ee8e74fcebadf8baaed46f8583ad1f
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2018-10-09 21:41:09 +0000

    remove tty

--- docker/autodeploy/build/build.sh
+++ docker/autodeploy/build/build.sh
@@ -10,16 +10,17 @@ docker build . -t "internal/autodeploy-builder:${DATE}"
 
 docker rm "autodeploy-builder" || true
 
-docker run -it \
+docker run \
 	--cap-add SYS_PTRACE \
 	-v /usr/portage:/usr/portage:ro \
 	-v /usr/portage/distfiles:/usr/portage/distfiles:rw \
 	-v /data/cuboxi-packages:/usr/portage/packages:rw \
-	-entrypoint="/bin/bash" \
 	--name "autodeploy-builder" \
 	"internal/autodeploy-builder:${DATE}" \
 		bash /container-specific-setup.sh
 
 docker commit \
 	"autodeploy-builder" \
-	"slangenmaier/autodeploy:${DATE}"
+	"slangenmaier/autodeploy:latest"
+
+docker tag "slangenmaier/autodeploy:latest" "slangenmaier/autodeploy:${DATE}"
