e88987de5c538462ebfcbd8b54b0276638c4439d
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2018-10-10 20:43:18 +0000

    alternative port for now

--- docker/jenkins/run/run.sh
+++ docker/jenkins/run/run.sh
@@ -13,7 +13,7 @@ docker run \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v /usr/bin/docker:/usr/bin/docker \
 	-v /mnt/full-data/vols/jenkins-home:/var/lib/jenkins/home:rw \
-	-p 8080:8080 \
+	-p 7070:8080 \
 	--entrypoint="/sbin/init" \
 	--name "jenkins" \
 	"slangenmaier/jenkins:latest"
