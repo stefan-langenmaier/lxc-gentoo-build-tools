a1c8eba6506f82fbb62f7e73ed3b3d257b10ab3e
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2018-10-10 20:45:49 +0000

    use absolute path for file mounts

--- docker/autodeploy/run/build-kernel.sh
+++ docker/autodeploy/run/build-kernel.sh
@@ -3,8 +3,8 @@ set -e
 set -x
 
 docker run \
-	-v internal-build-kernel.sh:/build-kernel.sh:ro \
-	-v kernel-config:/kernel-config:ro\
+	-v ${PWD}/internal-build-kernel.sh:/build-kernel.sh:ro \
+	-v ${PWD}/kernel-config:/kernel-config:ro\
 	-v /usr/portage:/usr/portage:ro \
 	-v /usr/portage/distfiles:/usr/portage/distfiles:rw \
 	-v /mnt/full-data/vols/cuboxi-packages:/usr/portage/packages:rw \
