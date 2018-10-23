b2d3758db4669bc8c0a29df63aa6d0aa37a1fa55
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2018-10-23 17:44:40 -0400

    run manchester nextcloud

---
+++ docker/nextcloud/run/run-manchester.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+docker run \
+	--tmpfs /run \
+	-v /usr/portage:/usr/portage:ro \
+        -v /usr/portage/distfiles:/usr/portage/distfiles:rw \
+        -v /mnt/full-data/vols/nextcloud-config/letsencrypt:/etc/letsencrypt:rw \
+        -v /mnt/full-data/vols/nextcloud-data:/data/nextcloud:rw \
+        -v /mnt/full-data/vols/nextcloud-config/nextcloud/config.php:/var/www/localhost/htdocs/nextcloud/config/config.php:rw \
+        -v /mnt/full-data/vols/nextcloud-config/nginx/nginx.conf:/etc/nginx/nginx.conf:rw \
+        -v /mnt/full-data/vols/nextcloud-config/php/fpm-php7.2/fpm.d/www.conf:/etc/php/fpm-php7.2/fpm.d/www.conf:rw \
+        -v /mnt/full-data/vols/nextcloud-config/mysql/my.cnf:/etc/mysql/my.cnf:rw \
+        -v /mnt/full-data/vols/nextcloud-db:/var/lib/mysql:rw \
+	-p 443:443 \
+	-p 80:80 \
+	--rm \
+	-d \
+        --name "nextcloud" \
+        "slangenmaier/nextcloud:2018-10-20" \
+	        /sbin/init
+
