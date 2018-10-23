5858930098f950af243a459b5bee713a2c736e6a
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2018-10-23 17:46:36 -0400

    run kodi manchester

---
+++ docker/kodi/run/run-manchester.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+set -x
+
+docker run \
+	--rm \
+	-d \
+	--net host \
+	--privileged \
+	-v /usr/portage:/usr/portage:ro \
+	-v /usr/portage/distfiles:/usr/portage/distfiles:rw \
+	-v /data/cuboxi-packages:/usr/portage/packages:rw \
+	-v /mnt/full-data/vols/nextcloud-data/christina.langenmaier/files/:/data/christina:ro \
+	-v /mnt/full-data/vols/nextcloud-data/matthew.hart/files/:/data/matthew:ro \
+	-v /mnt/full-data/vols/kodi-config:/root/.kodi:rw \
+	-entrypoint="/bin/bash" \
+	--name "kodi" \
+	"slangenmaier/kodi:2018-10-22" \
+		kodi
