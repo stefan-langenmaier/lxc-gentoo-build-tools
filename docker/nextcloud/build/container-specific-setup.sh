62f5f6b07d6a4cd66da5400944180e2ceb1ced3d
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2018-10-30 17:16:05 +0000

    ignore if no crontab

--- docker/nextcloud/build/container-specific-setup.sh
+++ docker/nextcloud/build/container-specific-setup.sh
@@ -9,7 +9,7 @@ rc-update add php-fpm default
 rc-update add mysql default
 
 #write out current crontab
-crontab -l > mycron
+crontab -l > mycron || true
 #echo new cron into cron file
 echo "*/15 * * * * /bin/su nginx -s /usr/bin/php /var/www/localhost/htdocs/nextcloud/cron.php" >> mycron
 echo "13 5 3 * * certbot renew --nginx" >> mycron
