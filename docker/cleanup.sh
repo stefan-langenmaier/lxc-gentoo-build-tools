dd926cb2018b6d13c38b87f8ee6c74d79553ae2c
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2018-11-23 18:50:42 +0000

    do more cleanup

--- docker/cleanup.sh
+++ docker/cleanup.sh
@@ -2,4 +2,7 @@
 set -e
 set -x
 
-docker image prune -f --filter until=768h # 31d
+docker image prune -f # all dangling
+docker image prune -f --all --filter until=768h # 31d
+
+docker container prune -f
