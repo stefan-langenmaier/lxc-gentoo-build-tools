2aac83c8f3658d0993b275b2356d4f635ac5e048
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2018-11-23 18:50:12 +0000

    change docker config file location

--- docker/jenkins/run/run.sh
+++ docker/jenkins/run/run.sh
@@ -12,6 +12,7 @@ docker run \
 	-v /mnt/full-data/cuboxi-packages:/usr/portage/packages:rw \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v /usr/bin/docker:/usr/bin/docker \
+	-v /root/.docker/config.json:/var/lib/jenkins/.docker/config.json \
 	-v /mnt/full-data/vols/jenkins-home:/var/lib/jenkins/home:rw \
 	-p 7070:8080 \
 	--entrypoint="/sbin/init" \
