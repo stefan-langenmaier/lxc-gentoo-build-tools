83e3c339fe3e3607b13c84e4da8b9b037e25f449
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2018-11-24 20:15:57 +0000

    kernel update to 4.19.4

--- docker/autodeploy/run/build-kernel.sh
+++ docker/autodeploy/run/build-kernel.sh
@@ -4,14 +4,20 @@ set -x
 
 OUTER_FOLDER=/root/lxc-gentoo-build-tools/docker/autodeploy/run
 
-docker run \
-	-v ${OUTER_FOLDER}/internal-build-kernel.sh:/build-kernel.sh:ro \
-	-v ${OUTER_FOLDER}/kernel-config:/kernel-config:ro\
-	-v autodeploy-exchange:/autodeploy-exchange:rw \
-	-v /usr/portage:/usr/portage:ro \
-	-v /usr/portage/distfiles:/usr/portage/distfiles:rw \
-	-v /mnt/full-data/vols/cuboxi-packages:/usr/portage/packages:rw \
-	--name "kernel-builder" \
-	"slangenmaier/autodeploy:latest" \
-		/bin/bash /build-kernel.sh || \
-docker start -a kernel-builder
+CNAME=kernel-builder
+
+if [[ $(docker ps -a --filter "name=^/$CNAME$" --format '{{.Names}}') != $CNAME ]]
+then
+	docker run \
+		-v ${OUTER_FOLDER}/internal-build-kernel.sh:/build-kernel.sh:ro \
+		-v ${OUTER_FOLDER}/kernel-config:/kernel-config:rw \
+		-v autodeploy-exchange:/autodeploy-exchange:rw \
+		-v /usr/portage:/usr/portage:ro \
+		-v /usr/portage/distfiles:/usr/portage/distfiles:rw \
+		-v /mnt/full-data/vols/cuboxi-packages:/usr/portage/packages:rw \
+		--name "$CNAME" \
+		"slangenmaier/autodeploy:latest" \
+			/bin/bash /build-kernel.sh
+else
+	docker start -a $CNAME
+fi
