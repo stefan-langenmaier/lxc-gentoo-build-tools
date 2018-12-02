678904024900a9bf0921bd507d4598251f09910e
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2018-12-02 13:48:10 +0000

    debug line for return code

--- docker/autodeploy/run/internal-build-image.sh
+++ docker/autodeploy/run/internal-build-image.sh
@@ -23,4 +23,5 @@ if [ ! -z ${TOKEN} ]; then
 	git config --global user.email "stefan.langenmaier@gmail.com"
 	git config --global user.name "Stefan Langenmaier"
 	bash create-tag.sh
+	echo "exit code of create-tag.sh:  $?"
 fi
