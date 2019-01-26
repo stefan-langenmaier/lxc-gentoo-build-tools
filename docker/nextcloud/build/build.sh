fa383cb2261b424ab0f4279f00de80b92ff11709
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2019-01-26 23:25:25 +0000

    faster nextcloud build

--- docker/nextcloud/build/build.sh
+++ docker/nextcloud/build/build.sh
@@ -8,20 +8,25 @@ DATE=`date +%Y-%m-%d`
 
 docker build . -t "internal/nextcloud-builder:latest"
 
-docker rm "nextcloud-builder" || true
+CNAME=nextcloud-builder
 
-docker run \
-	--cap-add SYS_PTRACE \
-	--tmpfs /run \
-	-v /usr/portage:/usr/portage:ro \
-	-v /usr/portage/distfiles:/usr/portage/distfiles:rw \
-	-v /mnt/full-data/vols/cuboxi-packages/:/usr/portage/packages:rw \
-	--name "nextcloud-builder" \
-	"internal/nextcloud-builder:latest" \
-		bash /container-specific-setup.sh
+if [[ $(docker ps -a --filter "name=^/$CNAME$" --format '{{.Names}}') != $CNAME ]]
+then
+	docker run \
+		--cap-add SYS_PTRACE \
+		--tmpfs /run \
+		-v /usr/portage:/usr/portage:ro \
+		-v /usr/portage/distfiles:/usr/portage/distfiles:rw \
+		-v /mnt/full-data/vols/cuboxi-packages/:/usr/portage/packages:rw \
+		--name $CNAME \
+		"internal/nextcloud-builder:latest" \
+			bash /container-specific-setup.sh
+else
+	docker start -a $CNAME
+fi
 
 docker commit \
-	"nextcloud-builder" \
+	$CNAME \
 	"slangenmaier/nextcloud:latest"
 
 docker tag "slangenmaier/nextcloud:latest" "slangenmaier/nextcloud:${DATE}"
--- docker/nextcloud/build/container-specific-setup.sh
+++ docker/nextcloud/build/container-specific-setup.sh
@@ -3,17 +3,25 @@ set -e
 set -x
 
 emerge -uDN world -j1
-webapp-config -I www-apps/nextcloud 14.0.6 -d nextcloud -s nginx
-rc-update add nginx default
-rc-update add php-fpm default
-rc-update add mysql default
+eselect news read
 
-#write out current crontab
-crontab -l > mycron || true
-#echo new cron into cron file
-echo "*/15 * * * * /bin/su nginx -s /usr/bin/php /var/www/localhost/htdocs/nextcloud/cron.php" >> mycron
-echo "13 5 3 * * certbot renew --nginx" >> mycron
-#install new cron file
-crontab mycron
-rm mycron
-rc-update add cronie default
+if webapp-config -li | grep nextcloud
+then
+	webapp-config -U www-apps/nextcloud 14.0.6 -d nextcloud -s nginx
+else
+	webapp-config -I www-apps/nextcloud 14.0.6 -d nextcloud -s nginx
+	rc-update add nginx default
+	rc-update add php-fpm default
+	rc-update add mysql default
+
+	#write out current crontab
+	crontab -l > mycron || true
+	#echo new cron into cron file
+	echo "*/15 * * * * /bin/su nginx -s /usr/bin/php /var/www/localhost/htdocs/nextcloud/cron.php" >> mycron
+	echo "13 5 3 * * certbot renew --nginx" >> mycron
+	#install new cron file
+	crontab mycron
+	rm mycron
+	rc-update add cronie default
+fi
+emerge --depclean
