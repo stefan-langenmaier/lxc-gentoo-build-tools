8d3b5ffe3713666a03e9b8070f42b6df8f4a94c8
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2019-04-09 19:20:03 +0000

    version bump nextcloud to 14.0.10

--- docker/push-images.sh
+++ docker/push-images.sh
@@ -9,4 +9,5 @@ docker push slangenmaier/gentoo-base-image:$DATE_M
 
 docker push slangenmaier/nextcloud:$DATE_D
 docker push slangenmaier/kodi:$DATE_D
+docker push slangenmaier/backup:$DATE_D
 docker push slangenmaier/jenkins:$DATE_D
