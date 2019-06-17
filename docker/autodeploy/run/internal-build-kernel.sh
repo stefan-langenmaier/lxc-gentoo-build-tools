e81d1cceb7b75479c9268dec07ddac837babb835
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2019-06-17 21:28:16 +0000

    update kernel config

--- docker/autodeploy/run/internal-build-kernel.sh
+++ docker/autodeploy/run/internal-build-kernel.sh
@@ -3,20 +3,6 @@ set -e
 set -x
 
 cd /root
-mkdir /lib/firmware/imx/sdma -p
-
-if [ ! -e "/lib/firmware/vpu_fw_imx6q.bin" ]
-then
-	wget http://www.nxp.com/lgfiles/NMG/MAD/YOCTO/firmware-imx-8.0.bin
-	dd if=firmware-imx-8.0.bin bs=37180 skip=1 | tar xj
-	cp firmware-imx-8.0/firmware/vpu/vpu_fw_imx6q.bin /lib/firmware
-fi
-
-if [ ! -e "/lib/firmware/imx/sdma/sdma-imx6q.bin" ]
-then
-	wget https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/imx/sdma/sdma-imx6q.bin
-	cp sdma-imx6q.bin /lib/firmware/imx/sdma/sdma-imx6q.bin
-fi
 
 emerge -u gentoo-sources
 emerge --depclean
