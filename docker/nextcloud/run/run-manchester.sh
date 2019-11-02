0bb4067d8cc9ef71c8e2e67bb51ab3f99b50198a
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2019-11-02 04:48:45 -0400

    update manchester config

--- docker/nextcloud/run/run-manchester.sh
+++ docker/nextcloud/run/run-manchester.sh
@@ -8,8 +8,8 @@ docker run \
         -v /mnt/full-data/vols/nextcloud-data:/data/nextcloud:rw \
         -v /mnt/full-data/vols/nextcloud-config/nextcloud/config.php:/var/www/localhost/htdocs/nextcloud/config/config.php:rw \
         -v /mnt/full-data/vols/nextcloud-config/nginx/nginx.conf:/etc/nginx/nginx.conf:rw \
-        -v /mnt/full-data/vols/nextcloud-config/php/fpm-php7.2/fpm.d/www.conf:/etc/php/fpm-php7.2/fpm.d/www.conf:ro \
-        -v /mnt/full-data/vols/nextcloud-config/php/fpm-php7.2/php.ini:/etc/php/fpm-php7.2/php.ini:ro \
+        -v /mnt/full-data/vols/nextcloud-config/php/fpm-php7.3/fpm.d/www.conf:/etc/php/fpm-php7.3/fpm.d/www.conf:ro \
+        -v /mnt/full-data/vols/nextcloud-config/php/fpm-php7.3/php.ini:/etc/php/fpm-php7.3/php.ini:ro \
         -v /mnt/full-data/vols/nextcloud-config/mysql/my.cnf:/etc/mysql/my.cnf:rw \
         -v /mnt/full-data/vols/nextcloud-db:/var/lib/mysql:rw \
 	-p 443:443 \
