ba06bb33a4be694a18f37e78dcb9548dbba2ba6b
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2019-11-04 13:41:08 +0000

    add PATH to crontab for nginx binary

--- docker/nextcloud/build/container-specific-setup.sh
+++ docker/nextcloud/build/container-specific-setup.sh
@@ -19,6 +19,7 @@ else
 	#write out current crontab
 	crontab -l > mycron || true
 	#echo new cron into cron file
+	echo "PATH=/usr/sbin" >> mycron
 	echo "*/15 * * * * /bin/su nginx -s /usr/bin/php /var/www/localhost/htdocs/nextcloud/cron.php" >> mycron
 	echo "13 5 * * * /usr/bin/certbot renew --nginx" >> mycron
 	#install new cron file
