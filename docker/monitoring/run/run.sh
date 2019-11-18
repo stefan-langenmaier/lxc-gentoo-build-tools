c82e0b8fc67feae7bef7b7271b9fd0afe4ea59b3
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2019-11-18 12:30:09 +0000

    add ssh folder

--- docker/monitoring/run/run.sh
+++ docker/monitoring/run/run.sh
@@ -14,6 +14,7 @@ docker run \
 	-v /mnt/full-data/vols/monitoring-config/alertmanager/config.yml:/etc/alertmanager/config.yml:rw \
 	-v /mnt/full-data/vols/monitoring-config/collectd/collectd.conf:/etc/collectd.conf:rw \
 	-v /mnt/full-data/vols/monitoring-config/create-ssh-tunnels.start:/etc/local.d/create-ssh-tunnels.start:rw \
+	-v /mnt/full-data/vols/monitoring-config/ssh:/root/.ssh:rw \
 	-v /mnt/full-data/vols/monitoring-data:/var/lib/prometheus/data:rw \
 	-p 9191:9090 \
 	-p 3000:3000 \
