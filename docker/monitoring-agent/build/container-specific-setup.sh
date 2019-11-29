abd271f453a5833dbd94689a0df45427d6c91d9a
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2019-11-29 10:53:36 +0000

    add disk access and logging for collectd

--- docker/monitoring-agent/build/container-specific-setup.sh
+++ docker/monitoring-agent/build/container-specific-setup.sh
@@ -2,3 +2,6 @@
 
 emerge -uDN world --changed-use --with-bdeps=y
 rc-update add collectd default
+usermod -a -G disk collectd
+touch /var/log/collectd.conf
+chown collectd:collectd /var/log/collectd.conf
