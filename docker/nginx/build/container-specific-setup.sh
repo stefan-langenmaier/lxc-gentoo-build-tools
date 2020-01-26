08aa1a9af1216eb7db223d5d2c7f494251680c3b
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-01-26 20:02:09 +0000

    add nginx build with proxy configuration

---
+++ docker/nginx/build/container-specific-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+set -x
+
+emerge -uDN --changed-use --with-bdeps=y --binpkg-changed-deps=y world -j4 --load-average 4
+eselect news read
+
+rc-update add nginx default
---
+++ docker/nginx/run/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+docker run \
+	--tmpfs /run \
+	--network airrow \
+	-v /usr/portage:/usr/portage:ro \
+        -v /usr/portage/distfiles:/usr/portage/distfiles:rw \
+        -v /data/container-packages:/usr/portage/packages:rw \
+        -v /mnt/full-data/vols/airrow/db-config/letsencrypt:/etc/letsencrypt:rw \
+        -v /mnt/full-data/vols/airrow/db-config/nginx/nginx.conf:/etc/nginx/nginx.conf:rw \
+	-p 443:443 \
+	-p 80:80 \
+	--rm \
+	-d \
+        --name "proxy" \
+        "slangenmaier/nginx:latest" \
+        /sbin/init
+
