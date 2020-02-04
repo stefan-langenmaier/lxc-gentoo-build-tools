13a4b2ed6487bab286b5140e4ee111882df05bdf
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-02-04 16:30:12 +0000

    enable shutdown

--- docker/gentoo-container-image/run/run.sh
+++ docker/gentoo-container-image/run/run.sh
@@ -3,21 +3,16 @@ set -e
 set -x
 #	--privileged \
 #	-v /sys/fs/cgroup:/sys/fs/cgroup:rw \
+#	--cap-add=SYS_BOOT \
+
 
 docker run -it \
 	--rm \
 	--tmpfs /run \
+	--tmpfs /tmp \
 	-v /usr/portage:/usr/portage:ro \
 	-v /usr/portage/distfiles:/usr/portage/distfiles:rw \
 	-v /mnt/full-data/vols/cuboxi-packages:/usr/portage/packages:rw \
-	-p 8080:8080 \
 	--entrypoint="/sbin/init" \
 	--name "gentoo-container" \
-	"slangenmaier/gentoo-container-image:latest" #\
-#		/bin/bash
-
-#docker commit \
-#	"nextcloud-builder" \
-#	"stefan-langenmaier/nextcloud:${DATE}"
-
-#docker rm "nextcloud-builder"
+	"slangenmaier/gentoo-container-image:latest"
