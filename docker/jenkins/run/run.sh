290c664aba0cfe181f0cb49a28503be23dfab37a
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-03-22 10:04:59 +0000

    update jenkins execution

--- docker/jenkins/run/run.sh
+++ docker/jenkins/run/run.sh
@@ -10,10 +10,13 @@ docker run \
 	-v /usr/portage:/usr/portage:ro \
 	-v /usr/portage/distfiles:/usr/portage/distfiles:rw \
 	-v /mnt/full-data/vols/cuboxi-packages:/usr/portage/packages:rw \
+	-v /mnt/full-data/vols/container-profiles:/mnt/full-data/vols/container-profiles:ro \
+	-v /mnt/full-data/vols/common-builder/nginx/:/mnt/full-data/vols/common-builder/nginx:rw \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v /usr/bin/docker:/usr/bin/docker \
 	-v /root/.docker/config.json:/var/lib/jenkins/.docker/config.json \
 	-v /mnt/full-data/vols/jenkins-home:/var/lib/jenkins/home:rw \
+	-v /mnt/full-data/vols/jenkins-home/workspace:/mnt/full-data/vols/jenkins-home/workspace:ro \
 	-p 7070:8080 \
 	--entrypoint="/sbin/init" \
 	--name "jenkins" \
