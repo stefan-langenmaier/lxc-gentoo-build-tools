e12693ecaed3930a845808401dfe7a83e64ebfe7
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-03-22 15:03:31 +0000

    switch mariadb to common build

---
+++ docker/mariadb/build/build-common.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cd "$(dirname "$0")"
+. ../../common-base.sh
+
+BNAME=mariadb-builder-instance
+INAME=slangenmaier/mariadb
+ROOTFS=/mnt/full-data/vols/common-builder/mariadb/
+
+start_builder_container
+
+install_base_system
+
+set +e
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} rc-update add mysql default'
+set -e
+
+create_image
+
+stop_container
