b1fe0acfabe8d0b22d34634f7a3e233752e8e46b
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-03-22 15:45:30 +0000

    switch monitoring-agent to common build

---
+++ docker/monitoring-agent/build/build-common.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cd "$(dirname "$0")"
+. ../../common-base.sh
+
+BNAME=monitoring-agent-builder-instance
+INAME=slangenmaier/monitoring-agent
+ROOTFS=/mnt/full-data/vols/common-builder/monitoring-agent/
+
+start_builder_container
+
+install_base_system
+
+set +e
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} rc-update add collectd default'
+# currently no user is created in the ROOTFS
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} useradd collectd'
+
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} usermod -a -G disk collectd'
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} touch /var/log/collectd.conf'
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} chown collectd:collectd /var/log/collectd.conf'
+set -e
+
+create_image
+
+stop_container
--- docker/monitoring-agent/build/container-specific-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-emerge -uDN world --changed-use --with-bdeps=y
-rc-update add collectd default
-usermod -a -G disk collectd
-touch /var/log/collectd.conf
-chown collectd:collectd /var/log/collectd.conf
