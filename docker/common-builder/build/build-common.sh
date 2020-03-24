65624546efa9c443e41ab2dce2036009a6b23df6
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-03-24 14:12:52 +0000

    add some build deps to the builder

--- docker/common-builder/build/build-common.sh
+++ docker/common-builder/build/build-common.sh
@@ -15,8 +15,11 @@ start_builder_container
 install_base_system
 
 set +e
-
 docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} ln -s -f ../../usr/portage/profiles/default/linux/arm/17.0/armv7a /etc/portage/make.profile'
+
+# the builder will need the build deps also installed, this might be incomplete
+docker exec -e ROOT=/build/rootfs/ -e PORTAGE_CONFIGROOT=/build/portage-configroot/ $BNAME bash -c "emerge -u1 virtual/os-headers sys-devel/bison sys-devel/binutils"
+
 set -e
 
 create_image
