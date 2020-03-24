430c9f422e20f5b8fc6d8e4d82cfee81973a2aa2
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-03-24 15:25:00 +0000

    this should install all necessary build deps

--- docker/common-builder/build/build-common.sh
+++ docker/common-builder/build/build-common.sh
@@ -17,8 +17,8 @@ install_base_system
 set +e
 docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} ln -s -f ../../usr/portage/profiles/default/linux/arm/17.0/armv7a /etc/portage/make.profile'
 
-# the builder will need the build deps also installed, this might be incomplete
-docker exec -e ROOT=/build/rootfs/ -e PORTAGE_CONFIGROOT=/build/portage-configroot/ $BNAME bash -c "emerge -u1 virtual/os-headers sys-devel/bison sys-devel/binutils"
+# the builder will need the build deps also installed
+docker exec -e ROOT=/build/rootfs/ -e PORTAGE_CONFIGROOT=/build/portage-configroot/ $BNAME bash -c "emerge -u --root-deps @world"
 
 set -e
 
