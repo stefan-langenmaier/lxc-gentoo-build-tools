048b201d2d327bda67a745de7687d95d1c913ab3
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-03-29 11:47:08 +0000

    fix gerbera build, downgrade to 1.3.5

--- docker/gerbera/build/build-common.sh
+++ docker/gerbera/build/build-common.sh
@@ -15,7 +15,12 @@ start_builder_container
 install_base_system
 
 set +e
+docker exec $BNAME bash -c 'emerge -u dev-libs/libfmt dev-libs/spdlog dev-libs/pugixml'
+docker exec -e ROOT=/build/rootfs/ -e PORTAGE_CONFIGROOT=/build/portage-configroot/ -e EGIT_COMMIT="4aa895993baf2e91f72265ac81004cfd2f0e50eb" $BNAME bash -c 'emerge -u gerbera'
 docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} rc-update add gerbera default'
+
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} groupadd --system --gid 429 gerbera'
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} useradd --system -d /var/lib/gerbera/ -s /sbin/nologin --uid 429 --gid 429 gerbera'
 set -e
 
 create_image
--- docker/gerbera/run/run.sh
+++ docker/gerbera/run/run.sh
@@ -9,6 +9,7 @@ docker run -it \
 	-d \
 	-v /usr/portage:/usr/portage:ro \
 	-v /usr/portage/distfiles:/usr/portage/distfiles:rw \
+	-v /mnt/full-data/vols/gerbera:/var/lib/gerbera:rw \
 	-v /mnt/full-data/vols/cuboxi-packages:/usr/portage/packages:rw \
 	-v /mnt/full-data/vols/music:/data/music:ro \
 	-v /mnt/full-data/vols/tv:/data/tv:ro \
