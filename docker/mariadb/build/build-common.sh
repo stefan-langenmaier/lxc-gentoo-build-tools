e893a70e61e3c01dff794eee99547c79f1622fdb
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-03-30 10:56:32 +0000

    git commit mariadb users

--- docker/mariadb/build/build-common.sh
+++ docker/mariadb/build/build-common.sh
@@ -16,6 +16,8 @@ install_base_system
 
 set +e
 docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} rc-update add mysql default'
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} groupadd --system --gid 60 mysql'
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} useradd --system -d /var/lib/mysql/ -s /sbin/nologin --uid 60 --gid 60 mysql'
 set -e
 
 create_image
