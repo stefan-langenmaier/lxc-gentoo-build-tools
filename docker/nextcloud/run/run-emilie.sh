bc72f5cdaef944d9de28e49215f49d9549fd114e
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-04-02 13:45:32 -0400

    add new start parameters

--- docker/nextcloud/run/run-emilie.sh
+++ docker/nextcloud/run/run-emilie.sh
@@ -3,16 +3,18 @@
 docker run \
 	--tmpfs /run \
         -v /mnt/full-data/vols/nextcloud-data:/data/nextcloud:rw \
-        -v /data/nextcloud-config/letsencrypt:/etc/letsencrypt:rw \
-        -v /data/nextcloud-config/nextcloud/config.php:/var/www/localhost/htdocs/nextcloud/config/config.php:rw \
-        -v /data/nextcloud-config/nginx/nginx.conf:/etc/nginx/nginx.conf:rw \
-        -v /data/nextcloud-config/php/fpm-php7.2/fpm.d/www.conf:/etc/php/fpm-php7.2/fpm.d/www.conf:ro \
-        -v /data/nextcloud-config/php/fpm-php7.2/php.ini:/etc/php/fpm-php7.2/php.ini:ro \
+        -v /mnt/full-data/vols/nextcloud-config/letsencrypt:/etc/letsencrypt:rw \
+        -v /mnt/full-data/vols/nextcloud-config/nextcloud/config.php:/var/www/localhost/htdocs/nextcloud/config/config.php:rw \
+        -v /mnt/full-data/vols/nextcloud-config/nginx/nginx.conf:/etc/nginx/nginx.conf:rw \
+        -v /mnt/full-data/vols/nextcloud-config/php/fpm-php7.3/fpm.d/www.conf:/etc/php/fpm-php7.3/fpm.d/www.conf:ro \
+        -v /mnt/full-data/vols/nextcloud-config/php/fpm-php7.3/php.ini:/etc/php/fpm-php7.3/php.ini:ro \
 	-p 443:443 \
 	-p 80:80 \
 	--rm \
 	-d \
+        --stop-signal SIGPWR \
         --name "nextcloud" \
         "slangenmaier/nextcloud:latest" \
 	        /sbin/init
-
+sleep 10
+docker exec nextcloud su - nginx -s /bin/bash -c 'cd /var/www/localhost/htdocs/nextcloud ; php occ upgrade'
