80677d5a03045b8b2a0e10492a8553c79e989134
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-05-03 10:46:06 +0000

    version bump nextcloud

--- docker/nextcloud/build/container-specific-setup.sh
+++ docker/nextcloud/build/container-specific-setup.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 
-NC_VERSION=16.0.9
+NC_VERSION=17.0.6
 INSTALL_PATH=$(ROOT=$ROOTFS webapp-config -li nextcloud $NC_VERSION)
 # if the correct version is installed we can skip the installation
 if [[ -z "$INSTALL_PATH" ]]
