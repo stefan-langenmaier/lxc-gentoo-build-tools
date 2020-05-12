2b1ce790607be79dc26401e043cad53e0b5a8e48
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-05-12 06:40:46 +0000

    fix ROOT for eselect

--- docker/kernel/build/build-common.sh
+++ docker/kernel/build/build-common.sh
@@ -15,7 +15,7 @@ start_builder_container
 install_base_system
 
 set +e
-docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'eselect kernel set 1'
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'ROOT=${ROOTFS} eselect kernel set 1'
 docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'cp /build/portage-configroot/kernel-config ${ROOTFS}/usr/src/linux/.config'
 docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'cd ${ROOTFS}/usr/src/linux ; make oldconfig ; make -j5 ; make dtbs'
 
