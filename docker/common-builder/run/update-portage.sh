3b97c76527c1ebad672e02fbdc61a9dcc5e3f836
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-05-13 19:11:45 +0000

    update emerge sync

--- docker/common-builder/run/update-portage.sh
+++ docker/common-builder/run/update-portage.sh
@@ -2,18 +2,10 @@
 set -e
 set -x
 
-docker run  \
+docker run \
 	--rm \
 	--tmpfs /run \
 	-v /usr/portage:/usr/portage:rw \
 	--name "update-portage" \
 	"slangenmaier/common-builder:latest" \
 		emerge --sync
-
-docker run  \
-	--rm \
-	--tmpfs /run \
-	-v /usr/portage:/usr/portage:rw \
-	--name "update-portage" \
-	"slangenmaier/common-builder:latest" \
-		egencache --update --repo gentoo
