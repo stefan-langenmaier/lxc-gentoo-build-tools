6f12c4ffd7a1b37d8202b3c48cb4e1f4a9f76c4c
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-05-16 23:08:51 +0000

    add files folder to proxy

--- docker/nginx/run/proxy.sh
+++ docker/nginx/run/proxy.sh
@@ -6,6 +6,7 @@ docker run \
 	-v /usr/portage:/usr/portage:ro \
         -v /usr/portage/distfiles:/usr/portage/distfiles:rw \
         -v /data/container-packages:/usr/portage/packages:rw \
+        -v /mnt/full-data/vols/files/:/data/files:rw \
         -v /mnt/full-data/vols/airrow/db-config/letsencrypt:/etc/letsencrypt:rw \
         -v /mnt/full-data/vols/airrow/db-config/nginx/nginx.conf:/etc/nginx/nginx.conf:rw \
 	-p 443:443 \
