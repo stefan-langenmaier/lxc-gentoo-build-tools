c1925e8abb5b910aed1274b32c10fa00929bb3fa
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-05-23 09:04:22 +0000

    update vfl start script

--- docker/nextcloud/run/run-vfl.sh
+++ docker/nextcloud/run/run-vfl.sh
@@ -8,15 +8,19 @@ docker run \
         -v /mnt/full-data/vols/nextcloud-data:/data/nextcloud:rw \
         -v /mnt/full-data/vols/nextcloud-config/nextcloud/config.php:/var/www/localhost/htdocs/nextcloud/config/config.php:rw \
         -v /mnt/full-data/vols/nextcloud-config/nginx/nginx.conf:/etc/nginx/nginx.conf:rw \
-        -v /mnt/full-data/vols/nextcloud-config/php/fpm-php7.2/fpm.d/www.conf:/etc/php/fpm-php7.2/fpm.d/www.conf:rw \
-        -v /mnt/full-data/vols/nextcloud-config/php/fpm-php7.2/php.ini:/etc/php/fpm-php7.2/php.ini:rw \
+        -v /mnt/full-data/vols/nextcloud-config/php/fpm-php7.3/fpm.d/www.conf:/etc/php/fpm-php7.3/fpm.d/www.conf:rw \
+        -v /mnt/full-data/vols/nextcloud-config/php/fpm-php7.3/php.ini:/etc/php/fpm-php7.3/php.ini:rw \
         -v /mnt/full-data/vols/nextcloud-config/mysql/my.cnf:/etc/mysql/my.cnf:rw \
         -v /mnt/full-data/vols/nextcloud-db:/var/lib/mysql:rw \
 	-p 443:443 \
 	-p 80:80 \
+	--stop-signal SIGPWR \
 	--rm \
 	-d \
         --name "nextcloud" \
         "slangenmaier/nextcloud:latest" \
 	        /sbin/init
+
+sleep 1
+docker exec nextcloud su - nginx -s /bin/bash -c 'while ! /etc/init.d/mysql status >/dev/null ; do echo waiting ; sleep 1 ; done ; while ! /etc/init.d/redis status >/dev/null ; do echo waiting ; sleep 1 ; done ; cd /var/www/localhost/htdocs/nextcloud ; php occ upgrade'
 
