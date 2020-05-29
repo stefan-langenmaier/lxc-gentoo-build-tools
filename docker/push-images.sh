61d8a14a94664a021c7cf3b0720cbe7db72e2f83
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-05-29 18:26:05 +0000

    update pushed images

--- docker/push-images.sh
+++ docker/push-images.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-set -e
 set -x
 
 DATE_M=`date +%Y-%m`
 DATE_D="${DATE_M}-01"
 
-docker push slangenmaier/gentoo-base-image:$DATE_M
+docker push slangenmaier/common-builder:$DATE_D
 
 docker push slangenmaier/nextcloud:$DATE_D
 docker push slangenmaier/kodi:$DATE_D
 docker push slangenmaier/backup:$DATE_D
 docker push slangenmaier/jenkins:$DATE_D
 docker push slangenmaier/monitoring-agent:$DATE_D
+docker push slangenmaier/monitoring:$DATE_D
+docker push slangenmaier/gerbera:$DATE_D
