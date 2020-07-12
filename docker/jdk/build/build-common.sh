742182c1f5bccb787d321494e6fb30a23a678870
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-07-12 05:00:37 +0000

    add eselect jvm workaround

--- docker/jdk/build/build-common.sh
+++ docker/jdk/build/build-common.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")"
 . ../../common-base.sh
 
 BNAME=jdk-builder-instance
-INAME=slangenmaier/jdk-slim
+INAME=slangenmaier/jdk
 ROOTFS=/mnt/full-data/vols/common-builder/jdk/
 
 start_builder_container
@@ -16,6 +16,9 @@ install_base_system
 
 set +e
 docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} update-ca-certificates'
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} mkdir /dev'
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} touch /dev/null'
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} eselect java-vm set system 1'
 set -e
 
 create_image
