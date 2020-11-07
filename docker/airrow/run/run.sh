efb849b9848bf22cbdccb1a9a104b7e54afdbfc9
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2020-11-07 11:55:11 +0000

    update airrow params

--- docker/airrow/run/run.sh
+++ docker/airrow/run/run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 docker run \
+	-d \
 	--rm \
 	--network airrow \
 	-v /mnt/full-data/vols/airrow/app/application.properties:/config/application.properties:rw \
-	-d \
 	-p 6060:8080 \
+	-e DEBUG=true \
+	-e SEARCH_WALK_DISTANCE=1500 \
+	-e SEARCH_MIN_ACCURACY=25 \
         --name "airrow" \
         "slangenmaier/airrow-jvm:latest" \
-	java -jar /deployments/app.jar
+	/opt/openjdk-bin-11/bin/java -jar /deployments/app.jar
