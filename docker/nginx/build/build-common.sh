99d3f581721deabaad8d753efbaf99656737dd33
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2021-02-07 19:43:02 +0000

    update nginx permissions and certbot

--- docker/nginx/build/build-common.sh
+++ docker/nginx/build/build-common.sh
@@ -12,11 +12,19 @@ ROOTFS=/mnt/full-data/vols/common-builder/nginx/
 
 start_builder_container
 
+set +e
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'groupadd --system --gid 999 nginx'
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'useradd --system -d /var/lib/nginx/ -s /sbin/nologin --uid 999 --gid 999 nginx'
+set -e
+
 install_base_system
 
 set +e
 docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} rc-update add nginx default'
 # currently no user is created in the ROOTFS
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} groupadd --system --gid 999 nginx'
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} useradd --system -d /var/lib/nginx/ -s /sbin/nologin --uid 999 --gid 999 nginx'
+
 docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} useradd nginx'
 set -e
 
