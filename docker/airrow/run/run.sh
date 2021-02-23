0401889cd66be99c843103b7b9903d5b206fdcc5
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2021-02-23 18:01:53 +0000

    add latest airrow container config

--- docker/airrow/run/run.sh
+++ docker/airrow/run/run.sh
@@ -5,10 +5,13 @@ docker run \
 	--rm \
 	--network airrow \
 	-v /mnt/full-data/vols/airrow/app/application.properties:/config/application.properties:rw \
+	-v /mnt/full-data/vols/airrow-data:/data:rw \
 	-p 6060:8080 \
 	-e DEBUG=true \
 	-e SEARCH_WALK_DISTANCE=1500m \
+	-e SEARCH_SCALE=0.9 \
 	-e SEARCH_MIN_ACCURACY=25 \
+	-e STORAGE_DIRECTORY=/data/ \
         --name "airrow" \
         "slangenmaier/airrow-jvm:latest" \
 	/opt/openjdk-bin-11/bin/java -jar /deployments/app.jar
