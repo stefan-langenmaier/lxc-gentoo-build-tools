61fc5c37aaae84b136e98b88b0be20a83473516a
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2021-04-02 11:58:29 +0000

    change jenkins homedir

--- docker/jenkins/run/run.sh
+++ docker/jenkins/run/run.sh
@@ -9,7 +9,7 @@ docker run \
 	--tmpfs /run \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v /usr/bin/docker:/usr/bin/docker \
-	-v /root/.docker/config.json:/home/jenkins/.docker/config.json \
+	-v /root/.docker/config.json:/var/lib/jenkins/.docker/config.json \
 	-v /mnt/full-data/vols/jenkins-home:/var/lib/jenkins/home:rw \
 	-v /mnt/full-data/vols/jenkins-home/workspace:/mnt/full-data/vols/jenkins-home/workspace:ro \
 	-p 7070:8080 \
