dcdc472f58115c4d3eeac1f9c68059762628761e
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2021-04-05 10:11:00 +0000

    dev null needs to exist before java is installed

--- docker/jdk/build/build-common.sh
+++ docker/jdk/build/build-common.sh
@@ -12,8 +12,6 @@ ROOTFS=/mnt/full-data/vols/common-builder/jdk/
 
 start_builder_container
 
-install_base_system
-
 set +e
 docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} mkdir /dev'
 docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} touch /dev/null'
@@ -21,6 +19,8 @@ docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} update-ca-c
 # no gentoo vm installed
 #docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} eselect java-vm set system 1'
 set -e
+
+install_base_system
 
 create_image
 
