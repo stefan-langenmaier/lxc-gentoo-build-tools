1dc6fedbe8a89ed8346afaf8d2dd5b065e6d719c
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2021-05-19 06:21:18 +0000

    enable binutils in common-builder

--- docker/common-builder/build/build-common.sh
+++ docker/common-builder/build/build-common.sh
@@ -15,11 +15,16 @@ RUNTIME=full
 
 start_builder_container
 
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'eselect binutils set 1'
+
 install_base_system
 
 set +e
 docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} ln -s -f ../../usr/portage/profiles/default/linux/arm/17.0/armv7a /etc/portage/make.profile'
 docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} mkdir -p /etc/portage/repos.conf'
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} mkdir -p /dev'
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} touch /dev/null'
+docker exec -e ROOTFS=/build/rootfs $BNAME bash -c 'chroot ${ROOTFS} eselect binutils set 1'
 
 docker cp gentoo.conf $BNAME:/build/rootfs/etc/portage/repos.conf/gentoo.conf
 set -e
